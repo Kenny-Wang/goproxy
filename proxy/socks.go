9435429f3b4d2e136f5529db089a4beac5c7306e
Shell.Xu <shell909090@gmail.com>
2018-09-01 16:47:49 +0800

    add back old socks5 impl, still working.

--- proxy/socks.go
+++ proxy/socks.go
@@ -1,12 +1,162 @@
 package proxy
 
 import (
-	"context"
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
 	"net"
 
-	socks5 "github.com/armon/go-socks5"
 	"github.com/shell909090/goproxy/netutil"
 )
+
+var (
+	ErrProtocol        = errors.New("protocol error")
+	ErrAuthMethod      = errors.New("auth method wrong")
+	ErrWrongFmt        = errors.New("connect packet wrong format")
+	ErrUnknownAddrType = errors.New("unknown addr type")
+	ErrIPv6            = errors.New("ipv6 not support yet")
+)
+
+func readLeadByte(reader io.Reader) (b []byte, err error) {
+	var c [1]byte
+
+	n, err := reader.Read(c[:])
+	if err != nil {
+		return
+	}
+	if n < 1 {
+		return nil, io.EOF
+	}
+
+	b = make([]byte, int(c[0]))
+	_, err = io.ReadFull(reader, b)
+	return
+}
+
+func readString(reader io.Reader) (s string, err error) {
+	b, err := readLeadByte(reader)
+	if err != nil {
+		return
+	}
+	return string(b), nil
+}
+
+func GetHandshake(reader *bufio.Reader) (methods []byte, err error) {
+	var c byte
+
+	c, err = reader.ReadByte()
+	if err != nil {
+		return
+	}
+	if c != 0x05 {
+		return nil, ErrProtocol
+	}
+
+	methods, err = readLeadByte(reader)
+	return
+}
+
+func SendHandshakeResponse(writer *bufio.Writer, status byte) (err error) {
+	_, err = writer.Write([]byte{0x05, status})
+	if err != nil {
+		return
+	}
+	return writer.Flush()
+}
+
+func GetUserPass(reader *bufio.Reader) (user string, password string, err error) {
+	c, err := reader.ReadByte()
+	if err != nil {
+		return
+	}
+	if c != 0x01 {
+		err = errors.New("Auth Packet Error")
+		return
+	}
+
+	user, err = readString(reader)
+	if err != nil {
+		return
+	}
+	password, err = readString(reader)
+	return
+}
+
+func SendAuthResult(writer *bufio.Writer, status byte) (err error) {
+	var buf []byte = []byte{0x01, 0x00}
+
+	buf[1] = status
+	n, err := writer.Write(buf)
+	if n != len(buf) {
+		return errors.New("send buffer full")
+	}
+	return writer.Flush()
+}
+
+func GetConnect(reader *bufio.Reader) (hostname string, port uint16, err error) {
+	var c byte
+
+	buf := make([]byte, 3)
+	_, err = io.ReadFull(reader, buf)
+	if err != nil {
+		return
+	}
+	if buf[0] != 0x05 || buf[1] != 0x01 || buf[2] != 0x00 {
+		err = ErrWrongFmt
+		return
+	}
+
+	c, err = reader.ReadByte()
+	if err != nil {
+		return
+	}
+
+	switch c {
+	case 0x01: // IP V4 address
+		logger.Debug("hostname in ipaddr mode.")
+		var buf [4]byte
+		_, err = io.ReadFull(reader, buf[:])
+		if err != nil {
+			return
+		}
+		ip := net.IP(buf[:])
+		hostname = ip.String()
+	case 0x03: // DOMAINNAME
+		logger.Debug("hostname in domain mode.")
+		hostname, err = readString(reader)
+		if err != nil {
+			return
+		}
+	case 0x04: // IP V6 address
+		err = ErrIPv6
+		logger.Error(err.Error())
+		return
+	default:
+		err = ErrUnknownAddrType
+		logger.Error(err.Error())
+		return
+	}
+
+	err = binary.Read(reader, binary.BigEndian, &port)
+	return
+}
+
+func SendConnectResponse(writer *bufio.Writer, res byte) (err error) {
+	var buf []byte = []byte{0x05, 0x00, 0x00, 0x01,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	// TODO: fix it if bind addr and port are needed.
+	// REF: https://zh.wikipedia.org/wiki/SOCKS
+	var n int
+
+	buf[1] = res
+	n, err = writer.Write(buf)
+	if n != len(buf) {
+		return io.ErrShortWrite
+	}
+	return writer.Flush()
+}
 
 type SocksProxy struct {
 	dialer     netutil.Dialer
@@ -28,31 +178,64 @@ func NewSocksProxy(dialer netutil.Dialer, addr, username, password string) (p *S
 	return
 }
 
-func (p *SocksProxy) Start() (err error) {
-	conf := &socks5.Config{
-		Dial: p.Dial,
-	}
-	server, err := socks5.New(conf)
+func (p *SocksProxy) Start() {
+	go netutil.ListenAndServe("tcp", p.listenaddr, p.ServeConn)
+}
+
+func (p *SocksProxy) ServeConn(conn net.Conn) {
+	defer conn.Close()
+
+	dstconn, err := p.SocksHandler(conn)
 	if err != nil {
-		logger.Error("fail to start socks5 server:", err.Error())
 		return
 	}
 
-	go func() {
-		logger.Info("socks server started")
-		err := server.ListenAndServe("tcp4", p.listenaddr)
-		if err != nil {
-			logger.Error(err.Error())
-		}
-		// If there have an error, socks will shutdown and not reboot.
-		// Let's see if there have any problem.
-		// Auto-reboot may cause loop, which I'm trying to avoid.
-	}()
+	netutil.CopyLink(conn, dstconn)
 	return
 }
 
-func (p *SocksProxy) Dial(ctx context.Context, network, addr string) (conn net.Conn, err error) {
-	logger.Info("connect to", network, addr)
-	conn, err = p.dialer.Dial(network, addr)
-	return
+func (p *SocksProxy) SocksHandler(conn net.Conn) (dstconn net.Conn, err error) {
+	logger.Debugf("connection come from: %s => %s",
+		conn.RemoteAddr(), conn.LocalAddr())
+
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	methods, err := GetHandshake(reader)
+	if err != nil {
+		return
+	}
+
+	method := byte(0xff)
+	for _, m := range methods {
+		if m == 0 {
+			method = 0
+		}
+	}
+	// TODO: username & password
+	SendHandshakeResponse(writer, method)
+	if method == 0xff {
+		err = ErrAuthMethod
+		logger.Error(err.Error())
+		return
+	}
+	logger.Debug("socks handshark ok")
+
+	hostname, port, err := GetConnect(reader)
+	if err != nil {
+		// general SOCKS server failure
+		SendConnectResponse(writer, 0x01)
+		return
+	}
+	logger.Debugf("dst: %s:%d", hostname, port)
+
+	dstconn, err = p.dialer.Dial("tcp", fmt.Sprintf("%s:%d", hostname, port))
+	if err != nil {
+		// Connection refused
+		SendConnectResponse(writer, 0x05)
+		return
+	}
+	SendConnectResponse(writer, 0x00)
+
+	return dstconn, nil
 }
