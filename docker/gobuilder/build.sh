e7ae403389ae97e2b86e701bc884b85d5b06000b
Shell.Xu <shell909090@gmail.com>
2018-11-23 11:29:56 +0800

    update golang version in gobuilder

--- docker/gobuilder/build.sh
+++ docker/gobuilder/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O docker/gobuilder/go-amd64.tar.gz "https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz"
+wget -O docker/gobuilder/go-amd64.tar.gz "https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz"
 sudo docker build -t gobuilder docker/gobuilder
 rm -f docker/gobuilder/go-amd64.tar.gz
--- docker/gobuilder32/build.sh
+++ docker/gobuilder32/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O docker/gobuilder32/go-386.tar.gz "https://storage.googleapis.com/golang/go1.9.2.linux-386.tar.gz"
+wget -O docker/gobuilder32/go-386.tar.gz "https://dl.google.com/go/go1.11.2.linux-386.tar.gz"
 sudo docker build -t gobuilder32 docker/gobuilder32/
 rm -f docker/gobuilder32/go-386.tar.gz
